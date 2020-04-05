9c8893e0c918f80cf44050413ca1f3b8bd79529d
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-05 22:25:04 +0000

    new model

--- muzero/config.py
+++ muzero/config.py
@@ -105,16 +105,16 @@ def make_centipede_config() -> MuZeroConfig:
 
     return MuZeroConfig(
         game=Centipede,
-        nb_training_loop=5,
-        nb_episodes=20,
-        nb_epochs=50,
+        nb_training_loop=20,
+        nb_episodes=10,
+        nb_epochs=10,
         network_args={'action_size': 18,
                       'state_size': 4,
                       'representation_size': (50, 32, 1),
                       'max_value': 27000},
         network=CentipedeNetwork,
         action_space_size=18,
-        max_moves=10000,
+        max_moves=200,
         discount=0.997,
         dirichlet_alpha=0.25,
         num_simulations=11,  # Odd number perform better in eval mode
